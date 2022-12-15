67c1b2b80d4d56708d02867fee2c3dc481d2b196
Albert Tang <tangalbert919@yahoo.com>
2022-12-15 16:43:56 -0600

    martini: Initial device tree conversion
    Change-Id: I3ef877cd8f291d93c2b89ac07af0ba8ef5daee06

--- lineage_martini.mk
+++ lineage_martini.mk
@@ -8,26 +8,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+# Inherit from martini device
+$(call inherit-product, device/oneplus/martini/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
+PRODUCT_NAME := lineage_martini
+PRODUCT_DEVICE := martini
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := MT2111
 
-PRODUCT_SYSTEM_NAME := OnePlus9
-PRODUCT_SYSTEM_DEVICE := OnePlus9
+PRODUCT_SYSTEM_NAME := OnePlus9RT
+PRODUCT_SYSTEM_DEVICE := OnePlus9RT
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus9-user 12 RKQ1.211119.001 R.202210251512 release-keys" \
+    PRIVATE_BUILD_DESC="MT2111_IND-user 13 TP1A.220624.014 R.c15e3d-80279-88263:user release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus9/OnePlus9:12/RKQ1.211119.001/R.202210251512:user/release-keys
+BUILD_FINGERPRINT := OnePlus/MT2111_IND/OP5155L1:13/TP1A.220624.014/R.c15e3d-80279-88263:user/release-keys
